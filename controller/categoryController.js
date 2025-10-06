27934f43ad35f23ee3022731416eb2e233f772c2
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-10-06 22:37:28 +0300

    Add a function that update specific category

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,13 +1,13 @@
 const slugify = require('slugify');
 const asyncHandler = require('express-async-handler');
-const CategoryModel = require('../models/CategoryModel');
+const Category = require('../models/CategoryModel');
 
 // @desc      Create category
 // @route     POST /api/categories
 // @access    Private
 exports.createCategory = asyncHandler(async (req , res) => {
     const {name} = req.body;
-    const newCategory = await CategoryModel.create({
+    const newCategory = await Category.create({
         name,
         slug : slugify(name)
     });
@@ -21,18 +21,33 @@ exports.getCategories = asyncHandler(async (req , res) => {
     const page = req.query.page * 1 || 1;
     const limit = req.query.limit *1 || 5;
     const skip = (page - 1) * limit;
-    const categories = await CategoryModel.find({}).skip(skip).limit(limit);
+    const categories = await Category.find({}).skip(skip).limit(limit);
     return res.status(200).json({results : categories.length, data : categories});
 });
 
 // @desc     Get specific category
 // @route    POST /api/categories/:id
-// @access   public
+// @access   Private
 exports.getCategory = asyncHandler(async (req , res) => {
     const {id} = req.params;
-    const category = await CategoryModel.findById(id);
+    const category = await Category.findById(id);
     if(!category){
-        return res.status(404).json({message : `Category for this id${id} not found`});
+        return res.status(404).json({message : `Category for this id ${id} not found`});
     };
     return res.status(200).json({data : category});
+});
+
+// @desc     Update specific category
+// @route    PUT /api/categories/id
+// @access   Private
+exports.updateCategory = asyncHandler(async (req , res) => {
+    const {id} = req.params;
+    const category = await Category.findByIdAndUpdate(id , {new : true});
+    if(!category){
+        return res.status(404).json({message : `Catgory for this id ${id} not found`});
+    };
+    return res.status(200).json({
+        message : `Category for this id ${id} updated successfully`,
+        updateCategory : category
+    });
 });
