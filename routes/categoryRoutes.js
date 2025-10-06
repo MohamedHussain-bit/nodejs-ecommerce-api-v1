e2fde1f25c4ac5232c06b853d689fd0b0855ec02
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-10-06 22:05:21 +0300

    add a parameter route for spcific get category

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const {createCategory , getCategories} = require('../controller/categoryController');
+const {
+        createCategory,
+        getCategories,
+        getCategory
+    } = require('../controller/categoryController');
 
 const router = express.Router();
 
 router.route('/')
     .post(createCategory)
     .get(getCategories)
+
+router.route('/:id')
+    .get(getCategory)
 
 module.exports = router;
