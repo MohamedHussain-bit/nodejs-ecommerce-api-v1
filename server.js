6900e49ba29ab0dbc82b39640768078434566c71
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-10-10 01:39:52 +0300

    Create class ApiError reuseable error

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const  mongoose = require('mongoose');
 
 const connectBD = require('./config/connectDB');
 const categoryRoutes = require('./routes/categoryRoutes')
+const ApiError = require('./utils/apiError');
 
 
 const PORT = process.env.PORT || 5000
@@ -26,12 +27,19 @@ app.use('/api/categories' , categoryRoutes);
 app.use((req , res , next) => {
     // const err = new Error(`cannot find this route : ${req.originalUrl}`);
     // next(err.message);
-    next(new ApiError('message' , statusCode));
+    next(new ApiError(`cannot find this route : ${req.originalUrl}` , 400));
 });
 
 // Global error handling middleware
 app.use((err , req , res , next) => {
-    res.status(500).json({err});
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+    res.status(err.statusCode).json({
+        status : err.status,
+        error : err,
+        message : err.message,
+        stack : err.stack,
+    });
 });
 
 mongoose.connection.once('open' , () => {
