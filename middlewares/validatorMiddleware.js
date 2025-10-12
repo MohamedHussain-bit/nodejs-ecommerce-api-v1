6fbbb82a907848f1d25b81283beb010174a961c4
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-10-12 19:18:25 +0300

    Add execlamiton marck in if condetion in validator middleware

--- middlewares/validatorMiddleware.js
+++ middlewares/validatorMiddleware.js
@@ -3,7 +3,7 @@ const {validationResult} = require('express-validator');
 // find the validation error
 const validatorMiddleware =(req , res , next) => {
     const errors = validationResult(req);
-    if(errors.isEmpty()){
+    if(!errors.isEmpty()){
         return res.status(400).json({errors:errors.array()});
     };
     next();
