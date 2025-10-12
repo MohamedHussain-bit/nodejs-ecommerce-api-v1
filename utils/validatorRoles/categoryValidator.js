3b9f5bac109b700b501ad9d100bfd9998779d65e
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-10-12 20:14:51 +0300

    Add create category validator

--- utils/validatorRoles/categoryValidator.js
+++ utils/validatorRoles/categoryValidator.js
@@ -5,3 +5,14 @@ exports.getCategoryValidator = [
     check('id').isMongoId().withMessage('Invalied category id'),
     validatorMiddleware
 ];
+
+exports.createCategoryValidator = [
+    check('name')
+        .notEmpty()
+        .withMessage('Category name required')
+        .isLength({min : 5})
+        .withMessage('Too short category name')
+        .isLength({max : 35})
+        .withMessage('Too long category name'),
+    validatorMiddleware
+];
