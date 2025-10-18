5539264ee3d4fff97b213d808d864b3d65ed7805
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-10-19 00:09:01 +0300

    Add route to get brand , update brand  and delete brand

--- routes/brandRoutes.js
+++ routes/brandRoutes.js
@@ -21,4 +21,9 @@ router.route('/')
     .post(createBrandValidator , createBrand)
     .get(getBrands)
 
+router.route('/:id')
+    .get(getBrandValidator , getBrand)
+    .put(updateBrandValidator , updateBrand)
+    .delete(deleteBrandValidator , deleteBrand)    
+
 module.exports = router;
