820ad4971abf4610c7d8b1025384466437d575b1
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-10-20 15:46:43 +0300

    Add handler to get list of product

--- controller/productController.js
+++ controller/productController.js
@@ -6,8 +6,19 @@ const ApiError = require('../utils/apiError');
 // @desc     Create product
 // @route    POST /api/products
 // @access   Private
-exports.CreateProduct = asyncHandler( async (req , res , next) => {
+exports.CreateProduct = asyncHandler( async (req , res) => {
     req.body.slug = slugify(req.body.title);
     const product = await Product.create(req.body);
     return res.status(201).json({data : product});
+});
+
+// @desc     Create product
+// @route    POST /api/products
+// @access   Private
+exports.getProducts = asyncHandler( async (req , res) => {
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 5;
+    const skip = (page - 1) * limit
+    const product = await Product.find({}).skip(skip).limit(limit);
+    return res.status(200).json({results : product.length, page, date : product});
 });
