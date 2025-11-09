3827f97221daa0baaedafc6664b1adff6b1fcff6
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-11-09 20:15:01 +0200

    Add user route and access all handler

---
+++ routes/userRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const {
+    createUser,
+    getUsers,
+    getUser,
+    updateUser,
+    deleteUser,
+} = require('../controller/userController');
+
+const router = express.Router();
+
+router.route('/')
+    .post(createUser)
+    .get(getUsers)
+
+router.route('/:id')
+    .get(getUser)
+    .put(updateUser)
+    .delete(deleteUser)
+
+module.exports = router;
