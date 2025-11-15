3367620d423b00d5429ec8e3e2cbaa0695f9dc9d
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-11-16 01:11:57 +0200

    Add login validation layer

--- utils/validatorRoles/authValidator.js
+++ utils/validatorRoles/authValidator.js
@@ -46,3 +46,17 @@ exports.signupValidator = [
         }),
     validatorMiddleware
 ];
+
+exports.loginValidator = [
+    check('email')
+        .notEmpty()
+        .withMessage('Email required')
+        .isEmail()
+        .withMessage('Invalide email address'),
+    check('password')
+        .notEmpty()
+        .withMessage('Password required')
+        .isLength({min : 6})
+        .withMessage('Too short password at least 6 caracters'),
+    validatorMiddleware
+];
