cbd61edfeebc88c78a8554cd4ac92a85817f1387
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-11-23 01:59:05 +0200

    Add options on handler to send email and send email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = (options) => {
+const sendEmail = async (options) => {
     // Create transporter (service that with send email)
     const transporter = nodemailer.createTransport({
         host : process.env.EMAIL_HOST,
@@ -11,6 +11,15 @@ const sendEmail = (options) => {
             pass : process.env.EMAIL_PASSWORD
         }
     });
+    // Define email options (like from , to , subject , text)
+    const options = {
+        from : 'E-shop App <godzela.1242006.gmail.com',
+        to : options.email,
+        subject : options.subject,
+        text : options.message,
+    };
+    // Send Email
+    await transporter.sendMail(options);
 };
 
 module.exports = sendEmail;
