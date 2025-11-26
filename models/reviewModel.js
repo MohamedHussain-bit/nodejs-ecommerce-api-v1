3635f8c54a1ba914ac8e7e9e3d1c0360192b6efb
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-11-27 00:20:47 +0200

    Add data on model review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,9 +1,26 @@
 const mongoose = require('mongoose');
 
-const revieSchema = new mongoose.Schema({
-
+const reviewSchema = new mongoose.Schema({
+        title : {
+            type : String,
+        },
+        ratings : {
+            type : Number,
+            min : [1 , 'Min ratings value is 1.0'],
+            max : [5 , 'Min ratings value is 5.0'],
+        },
+        user : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'User',
+            required : [true , 'Review must belong to user'],
+        },
+        product : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'Product',
+            require : [true , 'Review must belong to product'],
+        },
     },
     {timestamps : true}
 );
 
-module.exports = mongoose.model('Review' , revieSchema);
+module.exports = mongoose.model('Review' , reviewSchema);
