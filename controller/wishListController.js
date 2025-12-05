4d45fcef03bb26e8e825be8aa58691addffa9fe7
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-12-05 23:16:17 +0200

    Create handler to add product to wishlist

---
+++ controller/wishListController.js
@@ -0,0 +1,24 @@
+const asyncHandler = require('express-async-handler');
+
+const User = require('../models/userModel');
+const ApiErorr = require('../utils/apiError');
+
+// @desc     Add product to wishlist
+// @route    POST /api/wishlist
+// @access   Protected/user
+exports.addProductToWishlist = asyncHandler(async (req , res , next) => {
+    const user = await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $addToSet : {wishList : req.body.productId}
+        },
+        {
+            new : true
+        }
+    );
+    res.status(200).json({
+        status : 'Success',
+        message : 'Product added successfully to your wishlist',
+        data : user.wishList
+    });
+});
