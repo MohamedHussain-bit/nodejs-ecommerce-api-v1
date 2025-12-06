122b87a06a439a193105901e5dbf9b4d9995d4cc
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-12-06 16:56:35 +0200

    Create handler to removed address from user addresses user list

--- controller/addresseController.js
+++ controller/addresseController.js
@@ -22,3 +22,24 @@ exports.addAddress = asyncHandler(async (req , res , next) => {
         data : user.addresses
     });
 });
+
+// @desc     Remove address from user addreses list
+// @route    DELETE /api/addresses/:addressId
+// @access   Protected/user
+exports.removeAddress = asyncHandler(async (req , res , next) => {
+    // $pull => to remove address from addresses list
+    const user = await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $pull : {addresses : {_id : req.params.addressId}}
+        },
+        {
+            new : true
+        }
+    );
+    res.status(200).json({
+        status : 'Success',
+        message : 'Address removed successfully',
+        data : user.addresses
+    });
+});
