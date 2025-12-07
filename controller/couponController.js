bb418b8a39e9c0a45a11ceee2576a9ad44e99263
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-12-07 15:40:32 +0200

    Create handler to get list of coupon

--- controller/couponController.js
+++ controller/couponController.js
@@ -4,5 +4,10 @@ const Coupon = require('../models/couponModel');
 
 // @desc     Create coupon
 // @route    POST /api/coupon
-// @access   Prtcted/admin , manager
+// @access   Private/admin , manager
 exports.createCoupon = factory.createOne(Coupon);
+
+// @desc     Get list of coupon
+// @route    GET /api/coupon
+// @access   Private/admin , manager
+exports.getCoupons = factory.getList(Coupon);
