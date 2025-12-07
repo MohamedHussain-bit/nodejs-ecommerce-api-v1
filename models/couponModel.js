97713aba463c5dfd8a0dcd172454c846aa90b431
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-12-07 14:18:00 +0200

    Create coupon schema

---
+++ models/couponModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const couponSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        trim : true,
+        require : [true , 'Coupon name required'],
+        unique : true
+    },
+    expire : {
+        type : Date,
+        required : [true , 'Coupon expire time required']
+    },
+    discount : {
+        type : Number,
+        required : [true , 'Coupon discount value required']
+    }
+} , {timestamps : true});
+
+module.exports = mongoose.model('Coupon' , couponSchema);
