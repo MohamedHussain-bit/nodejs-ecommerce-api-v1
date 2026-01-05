53f0dd1fdacff2d210742bc67337129682bde806
MohamedHussain-bit <godzela.1242006@gmail.com>
2026-01-05 19:47:50 +0200

    Add get all and specific order to route

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,12 +2,34 @@ const express = require('express');
 
 const {
     createCashOrder,
+    getAllOrders,
+    getSpecificOrder,
+    filterOrderForLoggedUser,
 } = require('../controller/orderController');
 const autController = require('../controller/authController');
 
 const router = express.Router();
 
 router.route('/:cartId')
-    .post(autController.protected , autController.allowedTo('user') , createCashOrder)
+    .post(
+        autController.protected, 
+        autController.allowedTo('user'), 
+        createCashOrder
+    )
+
+router.route('/')
+    .post(
+        autController.protected, 
+        autController.allowedTo('user' , 'admin' , 'manager'),
+        filterOrderForLoggedUser,
+        getAllOrders
+    )
+
+router.route('/:id')
+    .post(
+        autController.protected , 
+        autController.allowedTo('user'), 
+        getSpecificOrder
+    )
 
 module.exports = router;
